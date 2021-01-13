84fb96b92a1742ecbc706778d9f71bd424492d85
katie <katie.robertson08@gmail.com>
2021-01-13 12:59:44 -0800

    adding products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,6 @@
 class Api::ProductsController < ApplicationController
+  def product_action
+    @message = "sanity check"
+    render "single_product.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
+  
+  namespace :api do
+    get "/product_path" => "products#product_action"
+  end
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+
 end
