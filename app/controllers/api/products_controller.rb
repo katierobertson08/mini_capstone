c77f281ced3475b69edc8dc7f0bac4d4ea9e3354
katie <katie.robertson08@gmail.com>
2021-01-18 11:42:59 -0800

    adds create, show and index in restful

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,12 +1,23 @@
 class Api::ProductsController < ApplicationController
-  def all_products_action
+  
+  def index
     @products = Product.all
     @products = Product.all # returns array of product hashes
-    render "all_products.json.jb"
+    render "index.json.jb"
   end
 
-  def first_product_action
-    @product = Product.first # returns first hash of product data
-    render "first_product.json.jb"
+  def show
+    @product = Product.first 
+    render "index.json.jb"
+  end
+
+  def create 
+    @product = Product.new(
+    name: params[:name],
+    price: params[:price],
+    description: params[:description],
+    )
+    @product.save
+    render "show.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   namespace :api do
     get "/all_products_path" => "products#all_products_action"
     get "/first_product_path" => "products#first_product_action"
+
+    get "/products" => "products#index"
+
+
+
+    get "/products/:id" => "products#show"
+    post "/products" => "products#create"
   end
 
 end
