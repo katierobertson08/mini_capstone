d5e66456cd79b299e7ef10a80686720f9ad0a506
katie <katie.robertson08@gmail.com>
2021-01-18 16:16:56 -0800

    adds configs initalizer

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   
   namespace :api do
-    get "/all_products_path" => "products#all_products_action"
-    get "/first_product_path" => "products#first_product_action"
-
     get "/products" => "products#index"
-
-
-
     get "/products/:id" => "products#show"
     post "/products" => "products#create"
     patch "/products/:id" => "products#update"
